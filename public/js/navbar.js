2d1c8d1193686fe10fb0e649fb79c607b0442a84
Chu Jang <cjang@ucsd.edu>
2016-10-27 12:59:13 -0700

    Working skeleton

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -20,3 +20,32 @@ document.getElementById("navMenu").innerHTML =
 			'</div>'+
 	  	'</div>'+
 	'</nav>';
+
+
+	/* fixed version 
+'<nav class="navbar navbar-inverse navbar-fixed-top"role="navigation">'+
+		'<div class="container">'+
+	        '<div class="navbar-header">'+
+	      		'<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">'+
+	        		'<span class="sr-only">Toggle navigation</span>'+
+			        '<span class="icon-bar"></span>'+
+			        '<span class="icon-bar"></span>'+
+			        '<span class="icon-bar"></span>'+
+	    		'</button>'+
+	    	'</div>'+
+	    	'<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">'+
+				'<ul class="nav navbar-nav">'+
+					'<li><a href="/">Home</a></li>'+
+					'<li><a href="/tasks/">Tasks</a></li>'+
+					'<li><a href="/group/">Group</a></li>'+
+					'<li><a href="/leaderboard/">Leaderboard</a></li>'+
+					'<li><a href="/other">Other</a></li>'+
+				'</ul>'+
+			'</div>'+
+	  	'</div>'+
+	'</nav>';
+
+
+
+
+	*/
