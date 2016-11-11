91daae482f140ddb1e16601441ad664231052a7b
Qing Hu <qih024@ucsd.edu>
2016-11-10 16:40:18 -0800

    working leaderboard base on json file

--- routes/leaderboard.js
+++ routes/leaderboard.js
@@ -1,6 +1,18 @@
 ﻿var jfile = require('../data1.json');
 exports.viewLeaderboard = function (req, res) {
+    //var count = (localStorage.getItem('currTaskCount') === null) ? 0 : (localStorage.getItem('currTaskCount'));
+    //for (var i = 0; i < count; i++) {
+    //    if (i >= jfile.Currency.length) {
+    //        var elem = {};
+    //        var 
+    //    }
+    //    var taskReward = localStorage.getItem('taskReward' + count);
+    //    jfile.Currency[i].UserCurrency = taskReward
+    //}
+    jfile.Currency.sort(function (obj1, obj2) {return obj2.UserCurrency - obj1.UserCurrency });
+    //jfile.Currency[0].UserName = "Kandarp";
     res.render('leaderboard', {
         dataJson: jfile
     });
+    //console.log(jfile.Currency);
 };
