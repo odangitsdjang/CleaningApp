5a5275664ba012bffb965b681f7b50d467ec493d
Chu Jang <cjang@ucsd.edu>
2016-11-14 06:50:51 -0800

    Possible heroku mongo fix

--- public/js/mongoUser.js
+++ public/js/mongoUser.js
@@ -1,6 +1,24 @@
 var bcrypt = require('bcryptjs');
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/CleaningApp');
+// localhost mode
+var uristring =
+    process.env.MONGOLAB_URI ||
+    process.env.MONGOHQ_URL ||
+    'mongodb://localhost/CleaningApp';
+
+    // The http server will listen to an appropriate port, or default to
+    // port 5000.
+    var theport = process.env.PORT || 5000;
+
+ mongoose.connect(uristring, function (err, res) {
+      if (err) {
+      console.log ('ERROR connecting to: ' + uristring + '. ' + err);
+      } else {
+      console.log ('Succeeded connected to: ' + uristring);
+      }
+    });
+
+
 
 var db = mongoose.connection;
 
